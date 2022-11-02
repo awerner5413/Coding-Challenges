510a7da50bbe50251aa7eef7f9aa9b6240e015d9
awerner5413 <werner.alex2@gmail.com>
2022-11-01 22:32:29 -0500

    adding partial solution

--- word-counter.py
+++ word-counter.py
@@ -1,11 +1,9 @@
 # Question:
-# Write a program to compute the frequency of the words from the input. The output should output after sorting the key alphanumerically.
-
+# Write a program to compute the frequency of the words from the input. The output should output after sorting the key 
+# alphanumerically.
 # Suppose the following input is supplied to the program:
-
 # New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3.
 # Then, the output should be:
-
 # 2:2
 # 3.:1
 # 3?:1
@@ -18,3 +16,16 @@
 # or:2
 # to:1
 
+sentence = input().split(' ')
+countList = {}
+
+for i in sentence:
+    a = sentence.count(i)
+    countList.update({i: a})
+
+print(countList)
+
+
+# TODO:
+# I can get the count of each item and then create a list of dictionaries and sort the list by key
+# currently provides what I want but isn't ordered and isn't printing as requested

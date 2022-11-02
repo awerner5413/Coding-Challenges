450971bfec27e44b01cceda6cc11dd58e0a33071
awerner5413 <werner.alex2@gmail.com>
2022-11-02 07:56:27 -0500

    new challenge files

---
+++ Square-Method.py
@@ -0,0 +1,11 @@
+# Question:
+# Write a method which can calculate square value of number
+# Hints:
+# Using the ** operator which can be written as n**p where means n^p
+
+a = int(input())
+
+def squared(num):
+    return num ** 2
+
+print(squared(a))

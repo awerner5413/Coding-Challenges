4a994375230d45a1f9e5fb540e874374db68abdc
awerner5413 <werner.alex2@gmail.com>
2022-12-28 16:04:04 -0600

    adding new JS challenge

--- Basic-JS.js
+++ Basic-JS.js
@@ -85,3 +85,23 @@ class Mamal extends Animal {
 }
 
 const cow1 = new Mamal ('Betsy', 'Cow', 'Purple')
+
+
+// #1) Check if this array includes the name "John".
+const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+dragons.includes('John')
+
+// #2) Check if this array includes any name that has "John" inside of it. If it does, return that
+// name or names in an array.
+const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+dragons.filter(name => name.includes('John'));
+
+// #3) Create a function that calulates the power of 100 of a number entered as a parameter
+function hundredPower(num) {
+  a = num**100;
+  console.log(a);
+}
+
+// #4) Useing your function from #3, put in the paramter 10000. What is the result?
+// Research for yourself why you get this result
+hundredPower(10000) === Infinity //useful to set limits if calculating something where the values become too large

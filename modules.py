5cf03fd1a2892905f7fd985258d1e29502007c66
awerner5413 <werner.alex2@gmail.com>
2023-01-12 22:17:33 -0600

    adding new challenge

---
+++ modules.py
@@ -0,0 +1,62 @@
+# Create a node class to hold values and links 
+# Create a linked list class to combine, add, and remove nodes from a list
+
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+    
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
+
+dean = Node("Dean")
+seamus = Node("Seamus", dean)
+
+gryffindor = LinkedList("Harry")
+gryffindor.insert_beginning("Ron")
+gryffindor.insert_beginning("Hermoine")
+gryffindor.insert_beginning("Neville")
+gryffindor.remove_node("Ron")
+gryffindor.get_head_node().get_next_node().get_next_node().set_next_node(seamus)
+
+print(gryffindor.stringify_list())
